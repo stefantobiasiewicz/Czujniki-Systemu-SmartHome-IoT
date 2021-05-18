cdbf17952582aaf31f1d654970403ad27b46a01e
Stefan <stefantobiasiewicz@gmail.com>
2021-05-18 02:48:33 +0200

    Dodanie pliku todo i określenie wstępnej ścierzki rozwoju systemu

--- IoT_8266_SENSORS/lib/sensor.h
+++ IoT_8266_SENSORS/lib/sensor.h
@@ -1,7 +1,11 @@
 #ifndef SENSOR__H
 #define SENSOR__H
 
+/*  Topologia struktury danych  */
+// definicje sensora
 #define SENSOR_TEMP 0x01
 #define SENSOR_HUM 0x02
+// definicje urządzenia wykonawczego
+#define DEVICE_SWITCH 0x81
 
 #endif
